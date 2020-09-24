0ff19d78601876be4c26571a6419d6548ba70acf
Olaf Weng <olafw@cqcp.com.tw>
2020-09-24 12:04:36 +0800

    v0.1.0--alpha.2

--- src/main.js
+++ src/main.js
@@ -1,39 +1,39 @@
-import Vue from 'vue'
-import Bulletins from './components/Bulletins'
+// import Vue from 'vue'
+// import Bulletins from './components/Bulletins'
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(Bulletins),
-}).$mount('#app')
+// new Vue({
+//   render: h => h(Bulletins),
+// }).$mount('#app')
 
-// import * as components from './components'
+import * as components from './components'
 
-// console.log('process', process.env)
+console.log('process', process.env)
 
-// const V2Lib = {
-//   install(Vue, options = {}) {
-//     console.log('Vue instance >>', Vue, options)
-//     console.log('import components >>', components)
+const V2Lib = {
+  install(Vue, options = {}) {
+    console.log('Vue instance >>', Vue, options)
+    console.log('import components >>', components)
 
-//     for (const componentName in components) {
-//       const component = components[componentName]
-//       console.log('component >>', component)
-//       Vue.component(component.name, component)
-//     }
+    for (const componentName in components) {
+      const component = components[componentName]
+      console.log('component >>', component)
+      Vue.component(component.name, component)
+    }
 
-//     // -- Define a global property
-//     Vue.VERSION = 'v0.1.0--alpha'
+    // -- Define a global property
+    Vue.VERSION = 'v0.1.0--alpha'
 
-//     // -- Create a mixin
-//     Vue.mixin({
-//       created () {
-//         console.log('Vue Instance is created >>', Vue)
-//       }
-//     })
-//   }
-// }
+    // -- Create a mixin
+    Vue.mixin({
+      created () {
+        console.log('Vue Instance is created >>', Vue)
+      }
+    })
+  }
+}
 
-// export default V2Lib
+export default V2Lib
 
-// if (typeof window !== 'undefined' && window.Vue) window.Vue.use(V2Lib)
+if (typeof window !== 'undefined' && window.Vue) window.Vue.use(V2Lib)
